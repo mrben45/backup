e3e111529611835a6160f150de8805b46fe33bc2
cyber indonesia <90544725+mrben45@users.noreply.github.com>
2023-02-19 15:22:42 +0700

    Add files via upload

---
+++ music.sh
@@ -0,0 +1,370 @@
+#!/usr/bin/bash
+
+clear
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+
+echo "$yellow "
+echo "#################$indigo MUSIC PLAYER SLOWED$yellow ################"
+echo "###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        1.selamat                               $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        2.boruto                                $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        3.angle                                 $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        4.lelolay                               $yellow###";
+echo "$yellow###$green   +$white------------------------------MP3🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        5.toxic friends                         $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        6.rasa                                  $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        7.beby family                           $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        8.love nwantiti                         $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple        9.dawdee                                $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple       10.broken                                $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple       11.dzhivan                               $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$purple       12.Play all music                        $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo        "###$red        13.EXIT $st                                $yellow###";
+echo "$yellow###$green   +$white---------------------------------🔊🔊🔊🔊$yellow   ###"
+echo "$yellow############$cyan MR$yellow #######$cyan 45$yellow #######$cyan CARD$yellow ##############"
+echo "$green                 🖥️  play audio " ;
+read -p "                  >>>> 💽 " pil;
+
+case $pil in
+1)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "🔴DJ SAD SELAMAT TINGGAL X TAHAN NIH LUKA TERBARU‼️Lana rmx ft dj haris Nugraha REMIX SLOWED 2022"
+echo "      📊📊📊📊📊📊      "
+cd
+cd ../usr/bin/data/;play-audio '🔴DJ SAD SELAMAT TINGGAL X TAHAN NIH LUKA TERBARU‼️Lana rmx ft dj haris Nugraha REMIX SLOWED 2022.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+2)
+echo "is playing   "
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "DJ KARA BORUTO BY KELUD TEAM OFFICIAL VIRAL TIK TOK TERBARU 2022 YANG KALIAN CARI !"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'DJ KARA BORUTO BY KELUD TEAM OFFICIAL VIRAL TIK TOK TERBARU 2022 YANG KALIAN CARI !.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+3)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "DJ SLOW🔥 __ DJ YANGLE VERSI SLOW🎧🔥__HARIS NUGRAHAa🔥"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'DJ SLOW🔥 __ DJ YANGLE VERSI SLOW🎧🔥__HARIS NUGRAHAa🔥.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+
+9)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "MORENA Remix DJdawdee 2013🔥"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'MORENA Remix DJdawdee 2013.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+10)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "Alter Bridge  Broken Wings🔥"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'Alter Bridge  Broken Wings.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+11)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "DZHIVAN  Автор🔥"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'DZHIVAN  Автор.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+4)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "Dj Lelolay X Akimilakuo - Remix Slow Beat Viral Tiktok 🎶"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'Dj Lelolay X Akimilakuo - Remix Slow Beat Viral Tiktok 🎶.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+5)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "DJ TOXIC FRIENDS SLOW BEAT VIRAL TIKTOK 2021"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'DJ TOXIC FRIENDS SLOW BEAT VIRAL TIKTOK 2021.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+6)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "DJ RASA YANG TERTINGGAL x DAPATKAH AKU MEMELUKNYA TIKTOK VIRAL REMIX FULL BASS TERBARU 2021"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'DJ RASA YANG TERTINGGAL x DAPATKAH AKU MEMELUKNYA TIKTOK VIRAL REMIX FULL BASS TERBARU 2021.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+7)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "DJ SLOW BABY FAMILY FRIENDLY by AJY ONE ZERO"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'DJ SLOW BABY FAMILY FRIENDLY by AJY ONE ZERO.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+8)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "DJ LOVE NWANTITI FUNKOT VIRAL TIK TOK TERBARU 2022 !"
+echo "      📊📊📊📊📊📊      "
+cd
+cd .. && cd usr/bin/data/;play-audio 'DJ LOVE NWANTITI FUNKOT VIRAL TIK TOK TERBARU 2022 !.mp3'
+cd
+echo "done"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+0)
+sleep 2.0
+echo "loading..."
+echo ""
+echo ""
+read -p " CONTINUE? "
+echo ""
+echo ""
+echo ""
+echo "prepare file data.zip on the internals"
+read -p " ready? "
+sleep 3.0
+cd
+cd storage/shared && mv data.zip /$HOME/../usr/bin && cd && cd ../usr/bin/;unzip data.zip && rm data.zip
+cd
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 0.5
+echo ""
+sleep 5.0
+echo "   ##   Author : mr45card  ##"
+sleep 2.0
+echo "enjoy playing music"
+sleep 2.0
+sh music.sh
+exit
+;;
+
+12)
+echo "is playing"
+sleep 1.0
+echo ""
+echo "playing mp3...     🎧🎧🎧🎧🎧"
+echo ""
+echo "all music"
+echo "..."
+sleep 1.0
+echo "🔴DJ SAD SELAMAT TINGGAL X TAHAN NIH LUKA TERBARU‼️Lana rmx ft dj haris Nugraha REMIX SLOWED 2022"
+echo "      📊📊📊📊📊📊      "
+cd
+cd ../usr/bin/data/;play-audio '🔴DJ SAD SELAMAT TINGGAL X TAHAN NIH LUKA TERBARU‼️Lana rmx ft dj haris Nugraha REMIX SLOWED 2022.mp3'
+sleep 2.0
+echo "DJ KARA BORUTO BY KELUD TEAM OFFICIAL VIRAL TIK TOK TERBARU 2022 YANG KALIAN CARI !"
+echo "      📊📊📊📊📊📊      "
+play-audio 'DJ KARA BORUTO BY KELUD TEAM OFFICIAL VIRAL TIK TOK TERBARU 2022 YANG KALIAN CARI !.mp3'
+sleep 2.0
+echo "DJ SLOW🔥 __ DJ YANGLE VERSI SLOW🎧🔥__HARIS NUGRAHAa"
+echo "      📊📊📊📊📊📊      "
+play-audio 'DJ SLOW🔥 __ DJ YANGLE VERSI SLOW🎧🔥__HARIS NUGRAHAa🔥.mp3'
+sleep 2.0
+echo "Dj Lelolay X Akimilakuo - Remix Slow Beat Viral Tiktok 🎶"
+echo "      📊📊📊📊📊📊      "
+play-audio 'Dj Lelolay X Akimilakuo - Remix Slow Beat Viral Tiktok 🎶.mp3'
+sleep 2.0
+echo "DJ TOXIC FRIENDS SLOW BEAT VIRAL TIKTOK 2021"
+echo "      📊📊📊📊📊📊      "
+play-audio 'DJ TOXIC FRIENDS SLOW BEAT VIRAL TIKTOK 2021.mp3'
+sleep 2.0
+echo "DJ RASA YANG TERTINGGAL x DAPATKAH AKU MEMELUKNYA TIKTOK VIRAL REMIX FULL BASS TERBARU 2021"
+echo "      📊📊📊📊📊📊      "
+play-audio 'DJ RASA YANG TERTINGGAL x DAPATKAH AKU MEMELUKNYA TIKTOK VIRAL REMIX FULL BASS TERBARU 2021.mp3'
+sleep 2.0
+echo "DJ SLOW BABY FAMILY FRIENDLY by AJY ONE ZERO"
+echo "      📊📊📊📊📊📊      "
+play-audio 'DJ SLOW BABY FAMILY FRIENDLY by AJY ONE ZERO.mp3'
+sleep 2.0
+echo "DJ LOVE NWANTITI FUNKOT VIRAL TIK TOK TERBARU 2022 !"
+echo "      📊📊📊📊📊📊      "
+play-audio 'DJ LOVE NWANTITI FUNKOT VIRAL TIK TOK TERBARU 2022 !.mp3'
+sleep 2.0
+echo "Alter Bridge  Broken Wings"
+echo "      📊📊📊📊📊📊      "
+play-audio 'Alter Bridge  Broken Wings.mp3'
+sleep 2.0
+echo "DZHIVAN  Автор.mp3"
+echo "      📊📊📊📊📊📊      "
+play-audio 'DZHIVAN  Автор.mp3'
+sleep 2.0
+echo "MORENA Remix DJdawdee 2013"
+echo "      📊📊📊📊📊📊      "
+play-audio 'MORENA Remix DJdawdee 2013.mp3'
+sleep 2.0
+echo                "🤭 "
+echo "done"
+echo "_________"
+read -p " back " back
+cd
+sleep 2.0
+echo ""
+echo ""
+echo "AUTHOR : MR45CARD"
+sleep 3.0
+echo "loading"
+sleep 1.0
+sh music.sh
+exit
+;;
+
+13)
+echo "$yellow"
+echo "#####################"
+sleep 1.0
+echo "thanks"
+echo ""
+echo ""
+echo ""
+exit
+;;
+*)
+echo ""
+echo ""
+echo "$red"
+echo "code error"
+sleep 2.0
+sh music.sh
+exit
+esac
+exit
+done
